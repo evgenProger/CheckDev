3eecf962d858b92a3a78af5c554fc4e55d0a6ff6
Dima-Stepanov <haoos@inbox.ru>
2023-09-05 00:05:53 +0300

    Переделать модуль site на SpringBoot

---
+++ services/sitemy/src/main/java/ru/job4j/sitemy/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ru.job4j.sitemy;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SiteApplication.class);
+	}
+
+}
