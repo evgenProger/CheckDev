dda164da21b6fb21faf01e48f0c1c3e44a4624e2
parsentev <parsentev@yandex.ru>
2023-09-09 21:03:42 +0300

    Сделать загрузку thymeleaf переделать на загрузку через spring boot #7

--- services/site/src/main/java/ru/job4j/site/SiteApplication.java
+++ services/site/src/main/java/ru/job4j/site/SiteApplication.java
@@ -1,17 +1,16 @@
 package ru.job4j.site;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SiteApplication {
     private static final String SITE = "http://localhost:8080";
-    private static final Logger LOG = LoggerFactory.getLogger(SiteApplication.class.getName());
 
     public static void main(String[] args) {
         SpringApplication.run(SiteApplication.class, args);
-        LOG.info("Go to -> :{}", SITE);
+        log.info("Go to -> :{}", SITE);
     }
 }
