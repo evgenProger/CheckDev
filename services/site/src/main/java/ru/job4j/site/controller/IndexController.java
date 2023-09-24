1e8202766ebf4a78228e9afdd8137df641e81ab9
Dima-Stepanov <haoos@inbox.ru>
2023-09-24 18:37:15 +0300

    Просмотр и редактирование пользователя #42

--- services/site/src/main/java/ru/job4j/site/controller/IndexController.java
+++ services/site/src/main/java/ru/job4j/site/controller/IndexController.java
@@ -6,49 +6,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import ru.job4j.site.dto.UserInfoDTO;
-import ru.job4j.site.service.AuthService;
 import ru.job4j.site.service.CategoriesService;
-
-import javax.servlet.http.HttpServletRequest;
-
-import static ru.job4j.site.controller.RequestResponseTools.getToken;
 
 @Controller
 @AllArgsConstructor
 @Slf4j
 public class IndexController {
-    private final AuthService authService;
     private final CategoriesService categoriesService;
 
     @GetMapping({"/", "index"})
-    public String getIndexPage(@RequestParam(value = "error", required = false) String error,
-                               Model model, HttpServletRequest request) throws JsonProcessingException {
+    public String getIndexPage(Model model) throws JsonProcessingException {
         RequestResponseTools.addAttrBreadcrumbs(model,
-                "Главная", "/index",
+                "Главная", "/",
                 "Категории", "/categories/"
         );
         model.addAttribute("categories", categoriesService.getAll());
-        String errorMessage = null;
-        if (error != null) {
-            errorMessage = "Email or Password is incorrect !!";
-        }
-        model.addAttribute("errorMessage", errorMessage);
-        model.addAttribute("userInfo", getUserInfo(request));
         return "index";
-    }
-
-    private UserInfoDTO getUserInfo(HttpServletRequest request) {
-        var token = getToken(request);
-        if (token == null) {
-            return null;
-        }
-        try {
-            return authService.userInfo(token);
-        } catch (Exception e) {
-            log.error("UserInfo data available. {}", e.getMessage());
-            return null;
-        }
     }
 }
---
+++ services/site/src/test/java/ru/job4j/site/controller/IndexControllerTest.java
@@ -0,0 +1,77 @@
+package ru.job4j.site.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.ui.ConcurrentModel;
+import ru.job4j.site.SiteApplication;
+import ru.job4j.site.domain.Breadcrumb;
+import ru.job4j.site.dto.CategoryDTO;
+import ru.job4j.site.service.CategoriesService;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+/**
+ * CheckDev пробное собеседование
+ * IndexControllerTest тесты на контроллер IndexController
+ *
+ * @author Dmitry Stepanov
+ * @version 24.09.2023 21:50
+ */
+@SpringBootTest(classes = SiteApplication.class)
+@AutoConfigureMockMvc
+class IndexControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private CategoriesService categoriesService;
+
+    private IndexController indexController;
+
+    @BeforeEach
+    void initTest() {
+        this.indexController = new IndexController(categoriesService);
+    }
+
+    @Test
+    void whenGetIndexPageThenReturnIndex() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    void whenGetIndexPageExpectModelAttributeThenOk() throws JsonProcessingException {
+        var catDTO1 = new CategoryDTO(1, "name1");
+        var catDTO2 = new CategoryDTO(2, "name2");
+        var listCatDTO = List.of(catDTO1, catDTO2);
+        when(categoriesService.getAll()).thenReturn(listCatDTO);
+        var listBread = List.of(new Breadcrumb("Главная", "/"),
+                new Breadcrumb("Категории", "/categories/"));
+        var model = new ConcurrentModel();
+
+        var view = indexController.getIndexPage(model);
+        var actualCategories = model.getAttribute("categories");
+        var actualBreadCrumbs = model.getAttribute("breadcrumbs");
+        var actualUserInfo = model.getAttribute("userInfo");
+
+        assertThat(view).isEqualTo("index");
+        assertThat(actualCategories).usingRecursiveComparison().isEqualTo(listCatDTO);
+        assertThat(actualBreadCrumbs).usingRecursiveComparison().isEqualTo(listBread);
+        assertThat(actualUserInfo).isNull();
+    }
+}
