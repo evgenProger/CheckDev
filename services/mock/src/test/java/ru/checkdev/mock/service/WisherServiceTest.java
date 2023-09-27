efdb958063bcf84ae95463d7d5d8ab31690c36c7
mihaylinsergey <semi999@mail.ru>
2023-09-27 14:53:14 +0300

    Поправить тесты в сервисе Mock. #69

--- services/mock/src/test/java/ru/checkdev/mock/service/WisherServiceTest.java
+++ services/mock/src/test/java/ru/checkdev/mock/service/WisherServiceTest.java
@@ -2,9 +2,11 @@ package ru.checkdev.mock.service;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.junit4.SpringRunner;
 import ru.checkdev.mock.Application;
 import ru.checkdev.mock.domain.Interview;
 import ru.checkdev.mock.domain.Wisher;
@@ -17,7 +19,7 @@ import ru.checkdev.mock.repository.WisherRepository;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@Disabled("It is used PostSQLDB. It should be h2.")
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 class WisherServiceTest {
 
--- services/mock/src/test/java/ru/checkdev/mock/web/WishersControllerTest.java
+++ services/mock/src/test/java/ru/checkdev/mock/web/WishersControllerTest.java
@@ -2,12 +2,14 @@ package ru.checkdev.mock.web;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.checkdev.mock.Application;
 import ru.checkdev.mock.domain.Interview;
@@ -20,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Disabled("It is used PostSQLDB. It should be h2.")
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @AutoConfigureMockMvc
 class WishersControllerTest {
