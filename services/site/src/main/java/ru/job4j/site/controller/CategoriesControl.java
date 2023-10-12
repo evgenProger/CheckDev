eca7e0a86479b1da82af34dae5e687e8204d9c39
mihaylinsergey <semi999@mail.ru>
2023-10-12 14:51:45 +0300

    Если нет сервиса Desc #210

--- services/site/src/main/java/ru/job4j/site/controller/CategoriesControl.java
+++ services/site/src/main/java/ru/job4j/site/controller/CategoriesControl.java
@@ -2,28 +2,28 @@ package ru.job4j.site.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.job4j.site.service.AuthService;
 import ru.job4j.site.service.CategoriesService;
-import ru.job4j.site.service.TopicsService;
-
 import javax.servlet.http.HttpServletRequest;
-
 import static ru.job4j.site.controller.RequestResponseTools.getToken;
 
 @Controller
 @RequestMapping("/categories")
 @AllArgsConstructor
+@Slf4j
 public class CategoriesControl {
     private final CategoriesService categoriesService;
     private final AuthService authService;
 
     @GetMapping("/")
     public String categories(Model model, HttpServletRequest req) throws JsonProcessingException {
-        model.addAttribute("categories", categoriesService.getAllWithTopics());
+        try {
+            model.addAttribute("categories", categoriesService.getAllWithTopics());
         var token = getToken(req);
         if (token != null) {
             var userInfo = authService.userInfo(token);
@@ -35,6 +35,12 @@ public class CategoriesControl {
                 "Категории", "/categories/"
         );
         model.addAttribute("current_page", "categories");
+        } catch (Exception e) {
+            RequestResponseTools.addAttrBreadcrumbs(model,
+                    "Главная", "/index"
+            );
+            log.error("Remote application not responding. Error: {}. {}, ", e.getCause(), e.getMessage());
+        }
         return "categories";
     }
 }
