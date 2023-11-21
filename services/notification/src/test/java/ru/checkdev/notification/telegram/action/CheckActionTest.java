c30f948198b4457b7414ff9cd9494a6490f0bfaa
parsentev <parsentev@yandex.ru>
2023-11-21 16:55:21 +0300

    Переписать тесты в слое бота на Fake #478

--- services/notification/src/test/java/ru/checkdev/notification/telegram/action/CheckActionTest.java
+++ services/notification/src/test/java/ru/checkdev/notification/telegram/action/CheckActionTest.java
@@ -1,48 +1,35 @@
 package ru.checkdev.notification.telegram.action;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
-import ru.checkdev.notification.NtfSrv;
 import ru.checkdev.notification.domain.UserTelegram;
+import ru.checkdev.notification.repository.InnerMessageRepositoryFake;
+import ru.checkdev.notification.repository.SubscribeTopicRepositoryFake;
+import ru.checkdev.notification.repository.UserTelegramRepositoryFake;
 import ru.checkdev.notification.service.InnerMessageService;
 import ru.checkdev.notification.service.UserTelegramService;
-import ru.checkdev.notification.telegram.service.TgCall;
+import ru.checkdev.notification.telegram.service.FakeTgCallConsole;
+import ru.checkdev.notification.telegram.service.TgCallNoConnect;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@TestPropertySource(locations = "classpath:application.properties")
-@SpringBootTest(classes = NtfSrv.class)
-@ExtendWith(SpringExtension.class)
-@AutoConfigureMockMvc
-@Disabled
 class CheckActionTest {
-
-    @Autowired
-    private UserTelegramService userTelegramService;
-
-    @Autowired
-    private InnerMessageService messageService;
-
-    @Autowired
-    private TgCall tgCall;
 
     @Test
     void whenNotChatId() {
+        var userTelegramService = new UserTelegramService(
+                new UserTelegramRepositoryFake(
+                        new SubscribeTopicRepositoryFake()
+                ));
         Chat chat = new Chat(1L, "type");
         userTelegramService.delete(1);
         Message message = new Message();
         message.setChat(chat);
-        CheckAction checkAction = new CheckAction(tgCall, userTelegramService, messageService);
+        CheckAction checkAction = new CheckAction(new FakeTgCallConsole(), userTelegramService,
+                new InnerMessageService(new InnerMessageRepositoryFake(), userTelegramService));
         checkAction.handle(message);
         BotApiMethod<Message> botApiMethod = checkAction.handle(message);
         SendMessage sendMessage = (SendMessage) botApiMethod;
@@ -53,13 +40,18 @@ class CheckActionTest {
 
     @Test
     void whenNotConnection() {
+        var userTelegramService = new UserTelegramService(
+                new UserTelegramRepositoryFake(
+                        new SubscribeTopicRepositoryFake()
+                ));
         Chat chat = new Chat(1L, "type");
         UserTelegram userTelegram = new UserTelegram(0, 10, chat.getId());
         userTelegramService.save(userTelegram);
         Message message = new Message();
         message.setChat(chat);
         message.setText("a@a.ru");
-        CheckAction checkAction = new CheckAction(tgCall, userTelegramService, messageService);
+        CheckAction checkAction = new CheckAction(new TgCallNoConnect(), userTelegramService,
+                new InnerMessageService(new InnerMessageRepositoryFake(), userTelegramService));
         BotApiMethod<Message> botApiMethod = checkAction.callback(message);
         SendMessage sendMessage = (SendMessage) botApiMethod;
         String n = System.lineSeparator();
--- services/notification/src/test/java/ru/checkdev/notification/telegram/action/UnNotifyActionTest.java
+++ services/notification/src/test/java/ru/checkdev/notification/telegram/action/UnNotifyActionTest.java
@@ -1,68 +1,57 @@
 package ru.checkdev.notification.telegram.action;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
-import ru.checkdev.notification.NtfSrv;
-import ru.checkdev.notification.domain.InnerMessage;
 import ru.checkdev.notification.domain.UserTelegram;
+import ru.checkdev.notification.repository.InnerMessageRepositoryFake;
+import ru.checkdev.notification.repository.SubscribeTopicRepositoryFake;
+import ru.checkdev.notification.repository.UserTelegramRepositoryFake;
 import ru.checkdev.notification.service.InnerMessageService;
 import ru.checkdev.notification.service.UserTelegramService;
-import ru.checkdev.notification.telegram.service.TgCall;
-
-import java.util.Optional;
+import ru.checkdev.notification.telegram.service.FakeTgCallConsole;
+import ru.checkdev.notification.telegram.service.TgCallNoConnect;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = NtfSrv.class)
-@AutoConfigureMockMvc
-@Disabled
 class UnNotifyActionTest {
-    @MockBean
-    private UserTelegramService userTelegramService;
-
-    @MockBean
-    private InnerMessageService messageService;
-
-    @Autowired
-    private TgCall tgCall;
 
     @Test
-    void whenNotChatId() {
+    void whenUnsubscribe() {
+        var userTelegramService = new UserTelegramService(
+                new UserTelegramRepositoryFake(
+                        new SubscribeTopicRepositoryFake()
+                ));
         Chat chat = new Chat(1L, "type");
         Message message = new Message();
         message.setChat(chat);
-        when(userTelegramService.findByChatId(chat.getId())).thenReturn(Optional.empty());
-        UnNotifyAction unNotifyAction = new UnNotifyAction(tgCall, userTelegramService, messageService);
+        var innerMessageService = new InnerMessageService(new InnerMessageRepositoryFake(), userTelegramService);
+        userTelegramService.save(new UserTelegram(1, 1, 1));
+        UnNotifyAction unNotifyAction = new UnNotifyAction(new FakeTgCallConsole(), userTelegramService, innerMessageService);
         unNotifyAction.handle(message);
         BotApiMethod<Message> botApiMethod = unNotifyAction.handle(message);
         SendMessage sendMessage = (SendMessage) botApiMethod;
-        String text = "Данный аккаунт Telegram на сайте не зарегистрирован";
-        assertThat(text).isEqualTo(sendMessage.getText());
+        assertThat(sendMessage.getText()).isEqualTo("Вы отписались от уведомлений");
     }
 
     @Test
     void whenNotConnection() {
+        var userTelegramService = new UserTelegramService(
+                new UserTelegramRepositoryFake(
+                        new SubscribeTopicRepositoryFake()
+                ));
         Chat chat = new Chat(1L, "type");
         UserTelegram userTelegram = new UserTelegram(0, 10, chat.getId());
-        when(userTelegramService.findByChatId(chat.getId())).thenReturn(Optional.of(userTelegram));
+        userTelegramService.save(userTelegram);
         Message message = new Message();
         message.setChat(chat);
         message.setText("a@a.ru");
-        when(messageService.saveMessage(any(InnerMessage.class))).thenThrow(new RuntimeException());
-        UnNotifyAction unNotifyAction = new UnNotifyAction(tgCall, userTelegramService, messageService);
+        var innerMessageService = new InnerMessageService(new InnerMessageRepositoryFake(), userTelegramService);
+        UnNotifyAction unNotifyAction = new UnNotifyAction(new TgCallNoConnect(), userTelegramService, innerMessageService);
         BotApiMethod<Message> botApiMethod = unNotifyAction.callback(message);
         SendMessage sendMessage = (SendMessage) botApiMethod;
-        String text = "Сервис не доступен попробуйте позже";
-        assertThat(text).isEqualTo(sendMessage.getText());
+        assertThat(sendMessage.getText()).isEqualTo("Сервис не доступен попробуйте позже");
     }
 }
