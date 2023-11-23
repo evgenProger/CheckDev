fb0ce62ac771eb5a7987a370e86cd0877fd54483
Arcady555 <parfenov7233@gmail.com>
2023-11-23 11:51:31 +0300

    #520 Добавить тесты в Telegram. И 2 ошибки - в NotifyAction и UnNotifyAction

--- services/notification/src/test/java/ru/checkdev/notification/telegram/action/UnNotifyActionTest.java
+++ services/notification/src/test/java/ru/checkdev/notification/telegram/action/UnNotifyActionTest.java
@@ -1,73 +1,64 @@
 package ru.checkdev.notification.telegram.action;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import ru.checkdev.notification.NtfSrv;
-import ru.checkdev.notification.domain.InnerMessage;
 import ru.checkdev.notification.domain.UserTelegram;
-import ru.checkdev.notification.service.InnerMessageService;
+import ru.checkdev.notification.repository.SubscribeTopicRepositoryFake;
+import ru.checkdev.notification.repository.UserTelegramRepositoryFake;
 import ru.checkdev.notification.service.UserTelegramService;
 import ru.checkdev.notification.telegram.SessionTg;
-import ru.checkdev.notification.telegram.service.TgCall;
+import ru.checkdev.notification.telegram.service.FakeTgCallConsole;
 
-import java.util.Optional;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-
-@SpringBootTest(classes = NtfSrv.class)
-@AutoConfigureMockMvc
-@Disabled
 class UnNotifyActionTest {
-    @MockBean
-    private UserTelegramService userTelegramService;
-
-    @MockBean
-    private InnerMessageService messageService;
-
-    @Autowired
-    private TgCall tgCall;
 
     @Test
     void whenNotChatId() {
         SessionTg sessionTg = new SessionTg();
         Chat chat = new Chat(1L, "type");
         Update update = new Update();
-        update.getMessage().setChat(chat);
-        when(userTelegramService.findByChatId(chat.getId())).thenReturn(Optional.empty());
-        UnNotifyAction unNotifyAction = new UnNotifyAction(sessionTg, tgCall, userTelegramService);
+        Message message = new Message();
+        update.setMessage(message);
+        message.setChat(chat);
+        var userTelegramService = new UserTelegramService(
+                new UserTelegramRepositoryFake(
+                        new SubscribeTopicRepositoryFake()
+                ));
+        UnNotifyAction unNotifyAction = new UnNotifyAction(sessionTg, new FakeTgCallConsole(), userTelegramService);
         unNotifyAction.handle(update);
         BotApiMethod botApiMethod = unNotifyAction.handle(update).get();
         SendMessage sendMessage = (SendMessage) botApiMethod;
         String text = "Данный аккаунт Telegram на сайте не зарегистрирован";
-        Assertions.assertEquals(text, sendMessage.getText());
+        Assertions.assertThat(text).isEqualTo(sendMessage.getText());
     }
 
     @Test
     void whenNotConnection() {
-        Update update = new Update();
-        SessionTg sessionTg = new SessionTg();
         Chat chat = new Chat(1L, "type");
-        UserTelegram userTelegram = new UserTelegram(0, 10, chat.getId());
-        when(userTelegramService.findByChatId(chat.getId())).thenReturn(Optional.of(userTelegram));
+        Update update = new Update();
         Message message = new Message();
+        update.setMessage(message);
+        message.setChat(chat);
+        SessionTg sessionTg = new SessionTg();
+        var userTelegramService = new UserTelegramService(
+                new UserTelegramRepositoryFake(
+                        new SubscribeTopicRepositoryFake()
+                ));
+        UserTelegram userTelegram = new UserTelegram(11, 10, chat.getId());
+        userTelegramService.save(userTelegram);
         message.setChat(chat);
         message.setText("a@a.ru");
-        when(messageService.saveMessage(any(InnerMessage.class))).thenThrow(new RuntimeException());
-        UnNotifyAction unNotifyAction = new UnNotifyAction(sessionTg, tgCall, userTelegramService);
-        BotApiMethod botApiMethod = unNotifyAction.handle(update).get();
+        UnNotifyAction unNotifyAction = new UnNotifyAction(sessionTg, new FakeTgCallConsole(), userTelegramService);
+        BotApiMethod<Message> botApiMethod = unNotifyAction.handle(update).get();
         SendMessage sendMessage = (SendMessage) botApiMethod;
-        String text = "Сервис не доступен попробуйте позже";
-        Assertions.assertEquals(text, sendMessage.getText());
+        String sl = System.lineSeparator();
+        String text = "Вы отписались от уведомлений";
+        assertThat(text).isEqualTo(sendMessage.getText());
     }
 }
