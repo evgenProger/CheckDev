32e04b7720a3321b90bc2bb6b8e744784a037994
Евгений Иванов <ivanov.evgen.v@gmail.com>
2026-03-25 08:50:00 +0300

    Монолитные системы [#338433 #560518]

--- services/notification/src/main/java/ru/checkdev/notification/domain/InnerMessage.java
+++ services/notification/src/main/java/ru/checkdev/notification/domain/InnerMessage.java
@@ -1,9 +1,6 @@
 package ru.checkdev.notification.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.*;
 import lombok.*;
 
 import java.sql.Timestamp;
